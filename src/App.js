926491e9b0d9d064d9564558fce381c3dcf7155b
garrettzhou <garrettzhou@futunn.com>
2023-01-17 23:36:48 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import { WagmiConfig, createClient, configureChains, goerli } from 'wagmi'
  
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from 'wagmi/providers/public'
- 
-import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
-import { InjectedConnector } from 'wagmi/connectors/injected'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
 
 import { Profile } from './components/Profile';
  
@@ -20,28 +15,6 @@ const { chains, provider, webSocketProvider } = configureChains(
 // Set up client
 const client = createClient({
   autoConnect: true,
-  connectors: [
-    new MetaMaskConnector({ chains }),
-    new CoinbaseWalletConnector({
-      chains,
-      options: {
-        appName: 'wagmi',
-      },
-    }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        qrcode: true,
-      },
-    }),
-    new InjectedConnector({
-      chains,
-      options: {
-        name: 'Injected',
-        shimDisconnect: true,
-      },
-    }),
-  ],
   provider,
   webSocketProvider,
 })
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,47 +1,69 @@
+import { useState } from 'react';
 import {
-    useAccount,
-    useConnect,
-    useDisconnect,
-    useEnsAvatar,
-    useEnsName,
-  } from 'wagmi'
-   
-  export function Profile() {
-    const { address, connector, isConnected } = useAccount()
-    const { data: ensAvatar } = useEnsAvatar({ address })
-    const { data: ensName } = useEnsName({ address })
-    const { connect, connectors, error, isLoading, pendingConnector } =
-      useConnect()
-    const { disconnect } = useDisconnect()
-   
-    if (isConnected) {
-      return (
-        <div>
-          <img src={ensAvatar} alt="ENS Avatar" />
-          <div>{ensName ? `${ensName} (${address})` : address}</div>
-          <div>Connected to {connector.name}</div>
-          <button onClick={disconnect}>Disconnect</button>
-        </div>
-      )
-    }
-   
-    return (
+  useAccount,
+  useConnect,
+  useDisconnect,
+  useEnsAvatar,
+  useEnsName,
+  useContractRead,
+  useContractWrite,
+  usePrepareContractWrite,
+} from 'wagmi'
+import { Contract } from 'ethers'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+
+import abi from './abi';
+
+export function Profile() {
+  const [total, setTotal] = useState(1);
+
+  const { address, isConnected } = useAccount()
+  const { data: ensAvatar } = useEnsAvatar({ address })
+  const { data: ensName } = useEnsName({ address })
+  const { connect, error } = useConnect({
+    connector: new MetaMaskConnector()
+  })
+  const { disconnect } = useDisconnect()
+
+  const { data } = useContractRead({
+    address: '0x436500248A0E7494dc12D953e7EA00B8A50B9710',
+    abi,
+    functionName: 'getInfo',
+  });
+
+
+  const { config } = usePrepareContractWrite({
+    address: '0x436500248A0E7494dc12D953e7EA00B8A50B9710',
+    abi,
+    functionName: 'mint',
+    args: [1]
+  })
+  const { write } = useContractWrite(config)
+  console.log('write:', write);
+  return (<>
+    {isConnected ?
       <div>
-        {connectors.map((connector) => (
-          <button
-            disabled={!connector.ready}
-            key={connector.id}
-            onClick={() => connect({ connector })}
-          >
-            {connector.name}
-            {!connector.ready && ' (unsupported)'}
-            {isLoading &&
-              connector.id === pendingConnector?.id &&
-              ' (connecting)'}
-          </button>
-        ))}
-   
+        <img src={ensAvatar} alt="头像" />
+        <div>当前用户: {ensName ? `${ensName} (${address})` : address}</div>
+        <button onClick={disconnect}>断开</button>
+        <br />
+        <br />
+        名称: {data?.deploymentConfig?.name} <br />
+        作者： {data?.deploymentConfig?.owner} <br />
+        价格: {data?.runtimeConfig?.publicMintPrice?.toString()} wei <br />
+        数量：<input type='number' value={total} onChange={e => setTotal(e.target.value)} min={1} max={2} /> <br />
+        <button onClick={() => write?.()}>购买(mint)</button>
+      </div> :
+      <div>
+        <button
+          onClick={() => connect()}
+        >
+          连接
+        </button>
+
         {error && <div>{error.message}</div>}
-      </div>
-    )
-  }
+      </div>}
+  </>
+  )
+
+}
